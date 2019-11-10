e5f3648dac3668840d01f403e1dd6bbdeac73984
MLopez <lopezjimenezmauro05@gmail.com>
2019-11-10 12:57:40 -0600

    added keyboard interrupt exception for close the reading of data

--- main.py
+++ main.py
@@ -6,11 +6,20 @@ serialConnection = 9600 #bits per second
 arduino = serial.Serial(usbport, serialConnection)
 time.sleep(2)
 
-while True:
-    value = arduino.readline()         # read a byte string
-    string_n_value = value.decode()  # decode byte string into Unicode  
-    string_value = string_n_value.rstrip() # remove \n and \r
-    print(string_value)
-    time.sleep(0.1)            # wait (sleep) 0.1 seconds
+try:
+    while True:
+        value = arduino.readline()         # read a byte string
+        string_n_value = value.decode()  # decode byte string into Unicode  
+        string_value = string_n_value.rstrip() # remove \n and \r
+        humidity = int(string_value)
+        if (humidity < 500):
+            print("wet: ", humidity)  
+        else:
+            print("NOT wet: ", humidity)
+        
+    
+        time.sleep(0.1)            # wait (sleep) 0.1 seconds
 
-#arduino.close()
+except KeyboardInterrupt:
+    print('-Closed data reading-')
+    arduino.close()
